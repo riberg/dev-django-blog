aa74553463fdba1a2487dffbc05cb102da9ce60b
riberg <den.radaeff@gmail.com>
2022-07-11 01:56:58 +1000

    add pagination

--- weblog/views.py
+++ weblog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.views import View
+from django.core.paginator import Paginator
+from .models import Post
 
 class MainView(View):
     def get(self, request):
-        return render(request, 'weblog/home.html')
+        posts = Post.objects.all()
+        paginator = Paginator(posts, 6)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'weblog/home.html', context={
+            'page_obj': page_obj
+        })
