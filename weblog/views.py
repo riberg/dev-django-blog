eb60f7b83ff3d9a1db12e5c92ddd941959f6509e
riberg <den.radaeff@gmail.com>
2022-07-11 02:37:44 +1000

    add post detail

--- weblog/views.py
+++ weblog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.core.paginator import Paginator
 from .models import Post
@@ -11,4 +11,11 @@ class MainView(View):
         page_obj = paginator.get_page(page_number)
         return render(request, 'weblog/home.html', context={
             'page_obj': page_obj
+        })
+
+class PostDetailView(View):
+    def get(self, reauest, slug):
+        post = get_object_or_404(Post, url=slug)
+        return render(reauest, 'weblog/post_detail.html', context={
+            'post': post
         })
