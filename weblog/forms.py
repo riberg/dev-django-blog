184bf85338297b9672f43ea297269eb680b64c0e
riberg <den.radaeff@gmail.com>
2022-07-12 02:08:31 +1000

    add feedback form

--- weblog/forms.py
+++ weblog/forms.py
@@ -43,9 +43,10 @@ class SignUpForm(forms.Form):
         username = self.cleaned_data['username']
         duplicate_user = User.objects.filter(username__iexact=username)
         if duplicate_user.exists():
-            raise forms.ValidationError("Пользователь с таким именем уже существует")
+            raise forms.ValidationError(
+                "Пользователь с таким именем уже существует")
         return username
-        
+
     def save(self):
         user = User.objects.create_user(
             username=self.cleaned_data['username'],
@@ -71,5 +72,41 @@ class SignInForm(forms.Form):
             'class': 'form-control mt-2',
             'id': 'inputPassword',
             'placeholder': 'Пароль',
+        })
+    )
+
+
+class FeedBackForm(forms.Form):
+    name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'id': 'name',
+            'placeholder': 'Ваше имя',
+        })
+    )
+    email = forms.CharField(
+        max_length=100,
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'id': 'email',
+            'placeholder': 'Ваша почта',
+        })
+    )
+    subject = forms.CharField(
+        max_length=200,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'id': 'subject',
+            'placeholder': 'Тема',
+        })
+    )
+    message = forms.CharField(
+        max_length=100,
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'id': 'message',
+            'rows': 2,
+            'placeholder': 'Ваше сообщение',
         })
     )
--- weblog/views.py
+++ weblog/views.py
@@ -1,10 +1,12 @@
+import re
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.core.paginator import Paginator
 from .models import Post
-from .forms import SignUpForm, SignInForm
+from .forms import SignUpForm, SignInForm, FeedBackForm
 from django.contrib.auth import login, authenticate
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.mail import send_mail, BadHeaderError
 
 
 class MainView(View):
@@ -61,4 +63,36 @@ class SignInView(View):
                 return HttpResponseRedirect('/')
         return render(request, 'weblog/signin.html', context={
             'form': form,
+        })
+
+
+class FeedBackView(View):
+    def get(self, request):
+        form = FeedBackForm()
+        return render(request, 'weblog/contact.html', context={
+            'form': form,
+            'title': 'Форма обратной связи'
+        })
+
+    def post(self, request):
+        form = FeedBackForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            from_email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(f'От {name} | {subject}', message, from_email, [''])
+            except BadHeaderError:
+                return HttpResponse('Невалидный заголовок')
+            return HttpResponseRedirect('success')
+        return render(request, 'weblog/contact.html', context={
+            'form': form,
+        })
+
+
+class SuccessView(View):
+    def get(self, request):
+        return render(request, 'weblog/success.html', context={
+            'title': 'Спасибо',
         })
